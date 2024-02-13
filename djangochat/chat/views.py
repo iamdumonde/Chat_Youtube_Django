63ef34a81aab802a64f8ada189927d12e2bf3d30
iamdumonde <martinaboumonde@gmail.com>
2024-02-13 17:27:54 +0100

    ajout de la méthode checkview dans views.py

--- djangochat/chat/views.py
+++ djangochat/chat/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+# importer les models
+from .models import *
 
 # Create your views here.
+
+# function to display home.html
 def home(request):
     return render(request, 'home.html')
+
+# function to display room.html
+def room(request):
+    pass
+
+# function permettant de vérifier les informations que l'utilisateur a entré dans le formulaire du home.html, l'on récupère les informations par la méthode POST
+def checkview(request):
+    room = request.POST['room_name']
+    username = request.POST['username']
+    
+    # vérifie si le room_name entré par l'utilisateur existe 
+    if Room.objects.filter(name = room).exists():
+        return redirect('/'+ room + '/?username' + username)
+    else:
+        new_room = Room.objects.create(name = room)
+        new_room.save()
+        return redirect('/'+ room + '/?username' + username) 
+        
